6f50579d892a596b468e5e245621efec87162b1e
André maia <andrefsm95@gmail.com>
2023-06-21 20:49:56 +0100

    feat add: section 6

--- frontend/src/components/sections/section6/style.js
+++ frontend/src/components/sections/section6/style.js
@@ -1,9 +1,50 @@
-import { Box } from "@mui/material";
+import { Box, ListItem, Typography } from "@mui/material";
 import styled from "styled-components";
+
 
 export const SBox = styled(Box)`
   border: 2px solid var(--blue);
   border-radius: 8px;
   box-shadow: 0px 3px 6px var(--gray);
   padding: 33px;
+  margin-top: 323px;
+`;
+
+export const TypographyTitle = styled(Typography)`
+&&{
+  font-size: 18px;
+  font-weight: 600;
+  font-family: Poppins SemiBold;
+  color:var(--black);
+  text-align: left;
+}
+`;
+
+export const TypographyText2 = styled(Typography)`
+  &&{
+    font-size: 24px;
+  font-weight: 500;
+  font-family: Poppins Medium;
+  color:var(--blue);
+  text-align: center;
+  }
+`;
+
+export const TypographyText = styled(Typography)`
+  &&{
+    font-size:18px;
+    font-family: Poppins Medium;
+    font-weight: 500;
+    color: var(--black);
+    text-align:left;
+  }
+`;
+
+export const SListItem = styled(ListItem)`
+  &&{
+    font-size: 16px;
+    font-family:Poppins Regular;
+    color: var(--black);
+    text-align:left;
+  }
 `;
