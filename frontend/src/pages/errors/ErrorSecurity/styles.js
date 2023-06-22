18ea9624c4c51159f5c74213210d9ebb437c1b20
AndreMaia98 <andre.maia@jcanao.pt>
2023-06-22 15:14:18 +0100

    feat fix: error security added instead rgpd

--- frontend/src/pages/errors/ErrorSecurity/styles.js
+++ frontend/src/pages/errors/ErrorSecurity/styles.js
@@ -1,1 +1,21 @@
+import { Typography } from "@mui/material";
 import styled from "styled-components";
+
+export const TypographyTitle = styled(Typography)`
+  && {
+    font-size: 26px;
+    font-family: Poppins SemiBold;
+    color: var(--white);
+    text-align: left;
+    margin-bottom: 32px;
+  }
+`;
+
+export const TypographyText = styled(Typography)`
+  && {
+    font-size: 16px;
+    font-family: Poppins Regular;
+    color: var(--white);
+    text-align: left;
+  }
+`;
