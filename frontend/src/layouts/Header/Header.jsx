6f990aff67f5fe9bb4470e684546c765dde8bfd7
AndreMaia98 <andre.maia@jcanao.pt>
2023-06-30 12:19:24 +0100

    feat add: menu hamburguer

--- frontend/src/layouts/Header/Header.jsx
+++ frontend/src/layouts/Header/Header.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { SButton, SContainer, Simg } from "./styles";
-import { Grid, Container } from "@mui/material";
+import { Grid, Container, Drawer, List, ListItem } from "@mui/material";
 import Logo from "../../assets/logo/LogoAlphaleme.svg";
 import { Link, useLocation } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/MenuRounded";
 
 export default function Header() {
   const [activeRoute, setActiveRoute] = useState("/");
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
     setActiveRoute(location.pathname);
   }, [location]);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <SContainer maxWidth={false}>
@@ -36,7 +42,7 @@ export default function Header() {
           <Grid
             item
             sx={{
-              display: "flex",
+              display: { xs: "none", md: "flex" },
               gap: { xs: "16px", sm: "16px" },
               height: "100%",
             }}
@@ -61,6 +67,58 @@ export default function Header() {
                 Cibersegurança
               </SButton>
             </Link>
+          </Grid>
+          <Grid item sx={{ display: { xs: "flex", md: "none" } }}>
+            <MenuIcon
+              onClick={toggleMobileMenu}
+              sx={{ width: "40px", height: "40px", color: "var(--blue)" }}
+            />
+            <Drawer
+              anchor="left"
+              open={isMobileMenuOpen}
+              onClose={toggleMobileMenu}
+              sx={{
+                "& .MuiDrawer-paper": {
+                  width: "300px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "start",
+                },
+              }}
+            >
+              <List>
+                <ListItem
+                  disablePadding
+                  sx={{
+                    marginBottom: "6px",
+                    width: "50%",
+                  }}
+                >
+                  <Link to="/rgpd">
+                    <SButton
+                      disableRipple
+                      disableFocusRipple
+                      activeRoute={activeRoute}
+                      route="/rgpd"
+                    >
+                      RGPD
+                    </SButton>
+                  </Link>
+                </ListItem>
+                <ListItem disablePadding>
+                  <Link to="/">
+                    <SButton
+                      disableRipple
+                      disableFocusRipple
+                      activeRoute={activeRoute}
+                      route="/"
+                    >
+                      Cibersegurança
+                    </SButton>
+                  </Link>
+                </ListItem>
+              </List>
+            </Drawer>
           </Grid>
         </Grid>
       </Container>
